7c408e8d6235e05f01e0786696c2ed7544ae8ed2
Bin Yeamin Chowdhury <binyeamin.chowdhury@gmail.com>
2021-02-09 23:31:54 +0600

    adde 'Post' model

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    posts = [
-        {
-            'author': 'John Doe',
-            'title': 'Test Title 1',
-            'content': 'First blog post.',
-            'date_posted': 'January 08, 2021'
-        },
-        {
-            'author': 'Doe John',
-            'title': 'Test Title 2',
-            'content': 'Second blog post.',
-            'date_posted': 'January 12, 2021'
-        }
-    ]
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title' : 'Blog | Home'
     }
     return render(request, 'blog/index.html', context)
