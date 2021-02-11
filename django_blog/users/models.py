6b9fe4a14a2d470d9b904425cde3e7e3be719682
Bin Yeamin Chowdhury <yeamin.inverse.ai@gmail.com>
2021-02-11 17:49:21 +0600

    added functionality for updating profile

--- django_blog/users/models.py
+++ django_blog/users/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_picks')
+    image = models.ImageField(default='default_user_profile.png', upload_to='profile_picks')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 100 or img.width > 100:
+            output_size = (100, 100)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
