b4cfff36ee57b01e1af98a060991f800a4d8b6bf
Bin Yeamin Chowdhury <binyeamin.chowdhury@gmail.com>
2021-02-14 07:48:31 +0600

    wip:added blog CRUD

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def index(request):
@@ -7,6 +8,17 @@ def index(request):
         'title' : 'Blog | Home'
     }
     return render(request, 'blog/index.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
